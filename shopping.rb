9932e6bdc558cc9e65f91acae57527b349cf9165
Clayton Nyakana <cnyakana@thoughtworks.com>
2014-11-07 13:29:22 +0300

    added items as Hash

--- shopping.rb
+++ shopping.rb
@@ -10,3 +10,15 @@ get "/names" do
 	@names = ["Mukiza", "Remo"]
 	erb :names
 end
+
+
+get "/items" do
+	@items = [
+		{	name: "Sugar", quantity:4, unit:"Kgs" },
+		{ name: "IceCream", quantity:7, unit:"Litres"},
+		{ name: "Bread", quantity:1, unit:"Loaf" },
+	]
+	erb :items
+end
+
+
